32b815f287ac3f71765acf56c9db11e41b60e25a
dangbb <51513363+dangbb@users.noreply.github.com>
2020-08-22 17:35:20 +0700

    Update find_euler_path.cpp

--- graph/find_euler_path.cpp
+++ graph/find_euler_path.cpp
@@ -1,9 +1,75 @@
-/// duong di euler la duong di qua tat ca cac canh, moi canh 1 lan
-/// chu trinh ueler la duong di euler co dinh dau trung voi dinh cuoi 
-/// do thi co duong di euler goi la do thi "nua euler"
-/// do thi co chu trinh euler goi la do thi "chu trinh euler"
+/// đường đi euler là đường đi qua tất cả các cạnh, mỗi cạnh một lần
+/// chu trình euler là đường đi euler có điểm đầu trùng với điểm cuối 
+/// đồ thị có đường đi euler gọi là đồ thị nửa euler
+/// đồ thị có chu trình euler gọi là đồ thị euler
 
-/// thuat toan: goi C la chu trinh euler can tim
-/// them 1 dinh u bat moi vao C
-/// se chon cac dinh ke voi u va chi chon canh cau la canh cuoi cung
-/// chào anh em
+/// thuật toán:
+
+#include <bits/stdc++.h>
+#define maxn 205
+#define f first
+#define s second
+
+using namespace std;
+
+typedef long long ll;
+
+int n, m, odd_num, s;
+
+int path[maxn][maxn], num[maxn];
+
+stack< int > euler_current_path;
+
+int main()
+{
+    freopen("a.inp","r",stdin);
+
+    cin >> n >> m;
+    for(int i=1;i<=m;i++)
+    {
+        int u, v;
+        cin >> u >> v;
+
+        path[u][v]++;
+        path[v][u]++;
+
+        ++num[u];
+        ++num[v];
+    }
+
+    s = 1;
+    for(int i=1;i<=n;i++)
+    {
+        if(num[i] % 2 == 1)
+        {
+            s = i;
+            break;
+        }
+    }
+
+    euler_current_path.push(s);
+
+    while(!euler_current_path.empty())
+    {
+        int v = euler_current_path.top();
+
+        for(int i=1;i<=n;i++)
+        {
+            if(path[v][i])
+            {
+                euler_current_path.push(i);
+                --path[v][i];
+                --path[i][v];
+                break;
+            }
+        }
+
+        if(v == euler_current_path.top())
+        {
+            /// can't find any vertex to add to the path
+            cout << euler_current_path.top() << " ";
+            euler_current_path.pop();
+        }
+    }
+}
+
